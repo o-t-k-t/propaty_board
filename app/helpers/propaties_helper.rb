acb7dd1ea37f5bd38a8dc4dece50692306e9dda8
helium <wetcondition@gmail.com>
2018-10-18 11:55:15 +0900

    課題のキャプチャと異なっている箇所の修正

--- app/helpers/propaties_helper.rb
+++ app/helpers/propaties_helper.rb
@@ -1,2 +1,12 @@
 module PropatiesHelper
+  def submit_button_name
+    case action_name
+    when 'new'
+      '登録する'
+    when 'edit', 'update'
+      '更新する'
+    else
+      raise "called by unexpected action #{action_name}"
+    end
+  end
 end
